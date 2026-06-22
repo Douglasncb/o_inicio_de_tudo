5ed4d2ff2d109c355338ef992585afa3d5099875
Douglas Bandeira <chagasd826@gmail.com>
2026-06-22 13:53:13 -0300

    finish api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 5 AS soma;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Tudo esta ocorrendo corretamente" });
-}
+  const updateAt = new Date().toISOString();
 
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.DB_NAME;
+
+  const databaseConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  //     "SELECT COUNT (*)::int FROM pg_stat_activity WHERE datname = current_database();",
+  const openedConnectionsValue = databaseConnectionsResult.rows.length;
+
+  console.log(openedConnectionsValue);
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependecies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: openedConnectionsValue,
+      },
+    },
+  });
+}
 export default status;
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,20 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString(); // Check if the date is valid
+  expect(responseBody.update_at).toEqual(parsedUpdateAt); // Ensure the date is in ISO format
+
+  expect(responseBody.dependecies.database.version).toEqual("16.0");
+  expect(responseBody.dependecies.database.max_connections).toEqual(100);
+  expect(responseBody.dependecies.database.opened_connections).toEqual(1);
+
+  expect(responseBody).not.toHaveProperty("password"); // Ensure that sensitive information is not included in the response
+  expect(responseBody).not.toHaveProperty("email");
+  expect(responseBody).not.toHaveProperty("secret_ley");
+
+  console.log(responseBody);
 });
